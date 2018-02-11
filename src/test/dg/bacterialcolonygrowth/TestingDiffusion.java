2fc2c1a27d190107025e480671fdd284b31e0932
Daniel Griffin <sc15dcg@leeds.ac.uk>
2018-02-11 01:30:38 +0000

    Create a unit tests file and test diffusion is correct after one time step.

---
+++ src/test/dg/bacterialcolonygrowth/TestingDiffusion.java
@@ -0,0 +1,32 @@
+package dg.bacterialcolonygrowth;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import sun.net.www.content.audio.x_aiff;
+
+public class TestingDiffusion {
+	
+	private CellularAutomataRules bacteriaSimulation;
+
+//	@Before
+//	public void setUp() throws Exception {	
+//	}
+
+//	@After
+//	public void tearDown() throws Exception {
+//	}
+	
+	// Test diffusion after one time step.
+	@Test
+	public void testNutrientDiffusesCorrectlyAfterOneTimestep() {
+		double[] initialNutrientLevels = new double[] {0,0,0,0,100};
+		bacteriaSimulation = new CellularAutomataRules(10, 10, initialNutrientLevels);
+		bacteriaSimulation.updateNutrientLevels();
+		assertEquals(bacteriaSimulation.returnNutrientLevelOfCell(4), 60, 0);
+	}
+	
+}
