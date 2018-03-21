f753a0b637fab45bbc1842c63317e834fea60f12
Daniel Griffin <sc15dcg@leeds.ac.uk>
2018-03-21 02:06:56 +0000

    Create class to manage the reading of the input file.

---
+++ src/dg/bacterialcolonygrowth/InputFileReader.java
@@ -0,0 +1,38 @@
+/* *****************************************************************************
+* Description: A class used to read the user's input file.
+*
+* Author: Daniel Griffin
+******************************************************************************/
+
+package dg.bacterialcolonygrowth;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InputFileReader {
+	
+	// Sets the parameters of the CullularAutomataBacteriaRules object that is passed to this method
+	// based on the values specified in the input file passed to this method.
+	public void setParametersFromInputFile(CellularAutomataBacteriaRules rules, File inputFile) {
+		// Used to store the contents of an individual line.
+        String line = null;
+
+        // Opens the file and reads each line.
+        try {   	
+        		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            
+            
+
+            reader.close();         
+        }
+        catch(IOException e) {
+            System.out.println("Error: Input file " + inputFile.getName() + " cannot be read.");                  
+        }
+    }
+}
